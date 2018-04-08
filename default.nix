c4c276eb184fd792f0a961aba40332aaedf98c27
Piotr Bogdan <ppbogdan@gmail.com>
2018-04-08 19:49:50 +0100

    Only provide static executables in the package.

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+with nixpkgs.pkgs.haskell.lib;
 let hsPkgs =
       nixpkgs.pkgs.haskell.packages.${compiler}.override {
         overrides= self: super: {
           python-pickle = self.callPackage ./deps/python-pickle.nix { };
          };
       };
-in hsPkgs.callPackage ./unrpa.nix { }
+in justStaticExecutables (hsPkgs.callPackage ./unrpa.nix { })
